ec11a9a43e0f11a626ab1bb9ef26ad8fe50742dd
Marco Willems (M17.1) <willima07@gmail.com>
2015-10-03 23:52:32 +0200

    Now able to download gzip-encoded webpages.

--- src/manga/downloader/Downloader.java
+++ src/manga/downloader/Downloader.java
@@ -10,9 +10,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
 
 /**
  *
@@ -21,12 +27,46 @@ import java.util.Scanner;
 public class Downloader {
 
     public static String getData(URL url) throws IOException {
-        StringBuilder strBuilder = new StringBuilder();
-        Scanner s = new Scanner(url.openStream());
-        while (s.hasNext()) {
-            strBuilder.append(s.nextLine()).append("\n");
+        URLConnection conn = url.openConnection();
+        String encoding = conn.getHeaderField("Content-Encoding");
+//        System.out.println(encoding);
+        if (encoding != null && encoding.equals("gzip")) {
+            Reader reader = null;
+            StringWriter writer = null;
+            try {
+                InputStream is = conn.getInputStream();
+                GZIPInputStream gzipIs = new GZIPInputStream(is);
+                reader = new InputStreamReader(gzipIs, "utf-8");
+                writer = new StringWriter();
+
+                char[] buffer = new char[32 * 1024];
+                int length = 0;
+                while ((length = reader.read(buffer)) != -1) {
+                    writer.write(buffer, 0, length);
+                }
+            } catch (IOException ex) {
+                Logger.getLogger(Downloader.class.getName()).log(Level.WARNING, null, ex);
+            } finally {
+                if (writer != null) {
+                    writer.close();
+                }
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+            if (writer != null) {
+                return writer.toString();
+            } else {
+                throw new IOException("Could not decode html from: " + url.getPath());
+            }
+        } else {
+            StringBuilder strBuilder = new StringBuilder();
+            Scanner s = new Scanner(url.openStream());
+            while (s.hasNext()) {
+                strBuilder.append(s.nextLine()).append("\n");
+            }
+            return strBuilder.toString();
         }
-        return strBuilder.toString();
     }
 
     public static void downloadFile(URL url, File dest) throws IOException {
